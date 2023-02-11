e7afb7ea065e12316431290ccc19c3e6a540ec9c
Konrad Brożyna <kondzio3002@gmail.com>
2023-02-11 18:27:13 +0100

    Add handlebars

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+
+app.engine('hbs', hbs());
+app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
   res.show = (name) => {
@@ -29,7 +33,11 @@ app.get('/info', (req, res) => {
 });
 
 app.get('/history', (req, res) => {
-  res.show('./views.history.html');
+  res.show('history.html');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
 });
 
 app.use((req, res) => {
